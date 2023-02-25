f58f89857170f91d068252ac4ba7b57ca0532bff
Miguelin69 <miguel2234505@correo.uis.edu.co>
2023-02-25 09:48:58 -0500

    se agrego un programa en python

---
+++ Numeros_Invertidos.py
@@ -0,0 +1,13 @@
+import math
+# Numeros_Invertidos
+print(--------NUMEROS INVERTIDOS-------)
+
+# input
+ N= int(input("digite el numero de tres digitos"))
+
+# processing
+
+NI= N%10
+p= N//10
+
+print("El numero invertido es" + str(NI))
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Numeros_Invertidos
 invertir un numero de 3 dígitos
+
+# Analisis
+
+# Diseño
+
+## Diagrama de flujo
+
+![Diagrama de flujo](diagramanumero.png "Diagrama de flujo")
